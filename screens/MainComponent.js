a63c7f42a770fb2be14e1f120a758a3e1c221caf
Janneth Gutierrez <jgutier1@gmail.com>
2023-09-21 03:18:34 -0400

    React Native: Week 2:   Exercise 1 Navigation icons.

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,9 @@ import DirectoryScreen from './DirectoryScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
+
 
 const Drawer = createDrawerNavigator();
 
@@ -22,9 +25,36 @@ const HomeNavigator = () => {
                 component={HomeScreen}
                 options={{ title: 'Home' }}
             />
-        </Stack.Navigator>
+        </Stack.Navigator>    
     );
 };
+
+
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='About'
+                component={AboutScreen}               
+            />
+        </Stack.Navigator>    
+    );
+};
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Contact'
+                component={ContactScreen}
+                options={{ title: 'Contact Us' }}
+            />
+        </Stack.Navigator>    
+    );
+};
+
 
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
@@ -72,9 +102,19 @@ const Main = () => {
                     component={DirectoryNavigator}
                     options={{ title: 'Directory' }}
                 />
+                <Drawer.Screen 
+                    name='About'
+                    component={AboutNavigator}                  
+                />
+                <Drawer.Screen 
+                    name='Contact'
+                    component={ContactNavigator}
+                    options={{title: 'Contact Us'}}
+                />
             </Drawer.Navigator>
         </View>
     );
+   
 };
 
 export default Main;
