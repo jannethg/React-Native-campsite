bd91f3efe9b9c7605e532bbfafae5320760567ab
Janneth Gutierrez <jgutier1@gmail.com>
2023-09-22 17:00:26 -0400

    React Native - Exercise: Adding Favorites
    you'll be able to toggle/save the heart favorite without losing the toggle when you click back.

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,49 +1,48 @@
-import RenderCampsite from '../features/campsites/RenderCampsite';
-import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import RenderCampsite from '../features/campsites/RenderCampsite';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    const comments = useSelector((state) => state.comments);    
-    const [favorite, setFavorite] = useState(false);
+    const comments = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
 
-    const renderCommentItem = ({ item })  => {
+    const renderCommentItem = ({ item }) => {
         return (
-          <View style={styles.commentItem}>
-            <Text style={{ fontsize: 14 }}>{item.text}</Text>
-            <Text style={{ fontsize: 12 }}>{item.rating} Stars</Text>
-            <Text style={{ fontSize: 12 }}>
-                {`-- ${item.author}, ${item.date}`}
-            </Text>
-          </View>
-        )
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
+            </View>
+        );
     };
 
     return (
         <FlatList
-        data={comments.commentsArray.filter(
-            (comment) => comment.campsiteId === campsite.id
-        )}
-        renderItem={renderCommentItem}
-        keyExtractor={(item) => item.id.toString()}
-        contentContainerStyle={{ 
-            marginHorizontal: 20, 
-            paddingVertical: 20 
-        }}
-
-        ListHeaderComponent={
-            <>
-                <RenderCampsite 
-                    campsite={campsite} 
-                    isFavorite={favorite}
-                    markFavorite={() => setFavorite(true) }
-                />
-                <Text style={styles.commentsTitle}>Comments</Text>
-            </>
-        }
-    />
+            data={comments.commentsArray.filter(
+                (comment) => comment.campsiteId === campsite.id
+            )}
+            renderItem={renderCommentItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite
+                        campsite={campsite}
+                        isFavorite={favorites.includes(campsite.id)}
+                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                    />
+                    <Text style={styles.commentsTitle}>Comments</Text>
+                </>
+            }
+        />
     );
 };
 
@@ -63,5 +62,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff'
     }
 });
-  
+
 export default CampsiteInfoScreen;
