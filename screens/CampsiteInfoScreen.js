9fc19ba073feb3aed6613e02dc89f28cbb37deb0
Janneth Gutierrez <jgutier1@gmail.com>
2023-09-23 09:44:07 -0400

    Week 2 Workshop Assignment : Task 1
    show ratings ins the comment section.

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -4,6 +4,7 @@ import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { Button, Modal } from 'react-native';
 import { useState } from 'react';
+import { Input, Rating } from 'react-native-elements';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
@@ -12,11 +13,37 @@ const CampsiteInfoScreen = ({ route }) => {
     const dispatch = useDispatch();
     const [showModal, setShowModal] = useState(false);
 
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+
+    const handleSubmit = () => {
+        const newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        };
+        console.log(newComment);
+        setShowModal(!showModal);
+    };
+
+    const resetForm = () => {
+        setRating(5);
+        setAuthor('');
+        setText('');
+    };
+
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
-                <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>               
+                <Rating                   
+                     startingValue={rating}
+                    imageSize={10}                   
+                    style={{ alignItems: 'flex-start', paddingVertical: '5%' }}
+                    readonly
+                />              
                 <Text style={{ fontSize: 12 }}>
                     {`-- ${item.author}, ${item.date}`}
                 </Text>
@@ -55,11 +82,42 @@ const CampsiteInfoScreen = ({ route }) => {
                 onRequestClose={() => setShowModal(!showModal)}
             >
                 <View style={styles.modal}>
+                    <Rating 
+                        showRating
+                        startingValue={rating}
+                        imageSize={40}
+                        onFinishRating={(rating)=> setRating(rating)}
+                        style={{paddingVertical: 10}}
+                    />
+                    <Input 
+                        placeholder='Author'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                        leftIconContainerStyle={{ padding: 10 }}
+                        onChangeText={(author)=> setRating(author)}                        
+                    />
+                    <Input 
+                        placeholder='Comment'
+                        leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                        leftIconContainerStyle={{ padding: 10 }}
+                        onChangeText={(text)=> setRating(text)}ss                        
+                    />
+                    <View  style={{margin: 10}} >
+                        <Button                        
+                            onPress={() => {
+                            handleSubmit();                            
+                            resetForm();
+                            }}
+                            color='#5637DD'
+                            title='Submit'
+                        />
+                        
+                    </View>
                     <View  style={{margin: 10}} >
                         <Button 
-                            onPress={() => 
-                            setShowModal(!showModal)
-                            }
+                            onPress={() => {
+                            setShowModal(!showModal);
+                            resetForm();
+                            }}
                             color='#808080'
                             title='Cancel'
                         />
