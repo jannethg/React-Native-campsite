da50e36aebda849b573394b249d981d738ccaece
Janneth Gutierrez <jgutier1@gmail.com>
2023-09-29 23:52:08 -0400

    React Native: Week 3 -  Exercise: Gestures Part 1
    gesture to swip right to left at dx -200

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,24 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
-import * as Animatable from 'react-native-animatable'
+import * as Animatable from 'react-native-animatable';
 
-
-const RenderCampsite =  (props) => {
+const RenderCampsite = (props) => {
     const { campsite } = props;
-    if (campsite)  {
+
+    const isLeftSwipe = ({ dx }) => dx < -200;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (isLeftSwipe(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' +
+                        campsite.name +
+                        ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () =>
+                                props.isFavorite
+                                    ? console.log('Already set as a favorite')
+                                    : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+        }
+    });
+
+    if (campsite) {
         return (
             <Animatable.View
-                animation='fadeInDown'
+                animation='fadeInDownBig'
                 duration={2000}
-                delay={2000}
+                delay={1000}
+                {...panResponder.panHandlers}
             >
                 <Card containerStyle={styles.cardContainer}>
                     <Card.Image source={{ uri: baseUrl + campsite.image }}>
                         <View style={{ justifyContent: 'center', flex: 1 }}>
-                            <Text style={styles.cardText} >
-                                {campsite.name}
-                            </Text>
+                            <Text style={styles.cardText}>{campsite.name}</Text>
                         </View>
                     </Card.Image>
                     <Text style={{ margin: 20 }}>{campsite.description}</Text>
@@ -36,17 +67,14 @@ const RenderCampsite =  (props) => {
                             }
                         />
                         <Icon
-                            name= 'pencil'
+                            name='pencil'
                             type='font-awesome'
                             color='#5637DD'
                             raised
                             reverse
-                            onPress={() => 
-                                props.onShowModal()
-                            }
+                            onPress={props.onShowModal}
                         />
                     </View>
-                    
                 </Card>
             </Animatable.View>
         );
@@ -59,7 +87,7 @@ const styles = StyleSheet.create({
         padding: 0,
         margin: 0,
         marginBottom: 20
-    }, 
+    },
     cardRow: {
         alignItems: 'center',
         justifyContent: 'center',
@@ -75,8 +103,6 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 20
     }
-
 });
 
 export default RenderCampsite;
-
